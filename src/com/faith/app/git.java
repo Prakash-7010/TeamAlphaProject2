0c0b413e500081471076c784082ff3e6000872e1
Prakash <prakashkhan976@gmail.com>
2022-11-10 15:58:38 +0530

    djfjndsgnd

--- src/com/faith/app/git.java
+++ src/com/faith/app/git.java
@@ -5,7 +5,7 @@ public class git {
 	public static void main(String[] args) {
 	System.out.println("I love you");
 	
-	System.out.println("hello");
+	System.out.println("hello faith infotech");
 
 }
 }
